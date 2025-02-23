7f923ac127be475dc71c1d91201bcf1779c491d0
Druxorey <druxorey@gmail.com>
2025-02-23 18:49:15 -0400

    feat(main): add logic to extract suspects list.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+struct coordinates {
+	int x;
+	int y;
+	int z;
+};
+
+struct spell {
+	string name;
+	int graphVertex;
+	string typeVertex;
+	int edges;
+	coordinates edgeCoordinates;
+};
+
+
 ifstream checkFile(string fileName) {
 	ifstream file(fileName);
 
@@ -29,12 +44,35 @@ int getSpellsQuantity(ifstream &file) {
 }
 
 
+void getSuspectsList(ifstream &file, string* &suspects, int &suspectsQuantity, int index = 0) {
+    if (file.eof()) {
+		suspectsQuantity--;
+        suspects = new string[suspectsQuantity];
+        return;
+    }
+
+    string suspectLine;
+    getline(file, suspectLine);
+    suspectsQuantity++;
+
+    getSuspectsList(file, suspects, suspectsQuantity, index + 1);
+
+	if (index < suspectsQuantity) {
+		suspects[index] = suspectLine;
+	}
+}
+
+
 int main(int argc, char *argv[]) {
 	ifstream spellFile = checkFile("spellList.in");
 	ifstream suspectFile = checkFile("underInvestigation.in");
 
     int spellsQuantity = getSpellsQuantity(spellFile);
+	spell spells[spellsQuantity];
 
+	string* suspects = nullptr;
+	int suspectsQuantity = 0;
+	getSuspectsList(suspectFile, suspects, suspectsQuantity);
 
     spellFile.close();
     suspectFile.close();
