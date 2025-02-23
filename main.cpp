26fd8d8a0fe596b9f78a4c74037efca1ce51b7a2
Druxorey <druxorey@gmail.com>
2025-02-23 17:42:10 -0400

    feat(main): add function to get spells quantity.

--- main.cpp
+++ main.cpp
@@ -15,9 +15,25 @@ ifstream checkFile(string fileName) {
 }
 
 
+int getSpellsQuantity(ifstream &file) {
+	string spellsLine = "";
+	// It has to be the first function to be called to get the quantity correctly
+	getline(file, spellsLine);
+	int spellsQuantity = stoi(spellsLine);
+
+	if (spellsQuantity < 1) {
+		cerr << "\e[0;31mError: The number of spells is out of range\e[0m" << '\n';
+		exit(1);
+	}
+	return spellsQuantity;
+}
+
+
 int main(int argc, char *argv[]) {
 	ifstream spellFile = checkFile("spellList.in");
 	ifstream suspectFile = checkFile("underInvestigation.in");
+
+    int spellsQuantity = getSpellsQuantity(spellFile);
 
 
     spellFile.close();
