173b13f95ab429ac59c588ce82d6a2e9b74d8e95
Druxorey <druxorey@gmail.com>
2025-02-25 21:50:59 -0400

    feat(main): implement function to fetch spells data.

--- main.cpp
+++ main.cpp
@@ -10,12 +10,39 @@ struct coordinates {
 };
 
 struct spell {
-	string name;
+	string suspectName;
 	int graphVertex;
 	string typeVertex;
-	int edges;
-	coordinates edgeCoordinates;
+	int edgesQuantity;
+	coordinates* edgeCoordinates;
 };
+
+
+
+
+
+// BEGIN OF DEBUG FUNCTIONS
+
+void printSpellsList(spell spells[], int spellsQuantity) {
+    for (int i = 0; i < spellsQuantity; i++) {
+        cout << "Suspect Name: " << spells[i].suspectName << endl;
+        cout << "Graph Vertex: " << spells[i].graphVertex << endl;
+        cout << "Type Vertex: " << spells[i].typeVertex << endl;
+        cout << "Number of Edges: " << spells[i].edgesQuantity << endl;
+        cout << "Edge Coordinates:" << endl;
+        for (int j = 0; j < spells[i].edgesQuantity; j++) {
+            cout << "  (" << spells[i].edgeCoordinates[j].x << ", "
+                 << spells[i].edgeCoordinates[j].y << ", "
+                 << spells[i].edgeCoordinates[j].z << ")" << endl;
+        }
+        cout << "------------------------" << endl;
+    }
+}
+
+//END OF DEBUG FUNCTIONS
+
+
+
 
 
 // Function to open a file and check if it opened correctly
@@ -66,6 +93,26 @@ void getSuspectsList(ifstream &file, string* &suspects, int &suspectsQuantity, i
 }
 
 
+// Function to get the list of spells from the file
+void getSpellsList(ifstream &file, spell spells[], int spellsQuantity) {
+	string spellLine = "";
+
+	for(int i = 0; i < spellsQuantity; i++) {
+		getline(file, spellLine);
+		spells[i].suspectName = spellLine;
+		file >> spells[i].graphVertex;
+		file >> spells[i].typeVertex;
+		file >> spells[i].edgesQuantity;
+		spells[i].edgeCoordinates = new coordinates[spells[i].edgesQuantity];
+		for(int j = 0; j < spells[i].edgesQuantity; j++) {
+			file >> spells[i].edgeCoordinates[j].x;
+			file >> spells[i].edgeCoordinates[j].y;
+			file >> spells[i].edgeCoordinates[j].z;
+		}
+	}
+}
+
+
 int main(int argc, char *argv[]) {
 	// Open spell and suspect files
 	ifstream spellFile = checkFile("spellList.in");
@@ -74,6 +121,10 @@ int main(int argc, char *argv[]) {
     // Get the quantity of spells
     int spellsQuantity = getSpellsQuantity(spellFile);
 	spell spells[spellsQuantity];
+
+	// Get the list of spells
+	getSpellsList(spellFile, spells, spellsQuantity);
+	printSpellsList(spells, spellsQuantity);
 
 	// Get the list of suspects
 	string* suspects = nullptr;
